9ce6a18f349e529d8bd2070f45855d3bde7626f0
Fabian Vogler <fabian@equivalence.ch>
2013-12-11 21:50:45 +0100

    Documented audio channels

--- arduino/fasnacht/fasnacht.ino
+++ arduino/fasnacht/fasnacht.ino
@@ -34,7 +34,7 @@ void loop()
     count = 0;
 
     for (x = 0; x < 500; x++) {
-        value = analogRead(3);
+        value = analogRead(2); // 2 = Audio links, 3 = Audio rechts
         if (value > highest) {
             highest = value;
         }
