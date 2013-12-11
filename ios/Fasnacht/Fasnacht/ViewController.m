1d3c898165acdde2fd251a99616a150870415a67
Fabian Vogler <fabian@equivalence.ch>
2013-12-11 23:02:50 +0100

    Implemented sliders and labels

--- ios/Fasnacht/Fasnacht/ViewController.m
+++ ios/Fasnacht/Fasnacht/ViewController.m
@@ -10,6 +10,16 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UISlider *sliderB;
+@property (weak, nonatomic) IBOutlet UISlider *sliderC;
+@property (weak, nonatomic) IBOutlet UISlider *sliderD;
+@property (weak, nonatomic) IBOutlet UISlider *sliderE;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelB;
+@property (weak, nonatomic) IBOutlet UILabel *labelC;
+@property (weak, nonatomic) IBOutlet UILabel *labelD;
+@property (weak, nonatomic) IBOutlet UILabel *labelE;
+
 @end
 
 @implementation ViewController
@@ -25,5 +35,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)sliderChangedB:(UISlider *)slider {
+	self.labelB.text = [NSString stringWithFormat:@"%.0f Hz", slider.value];
+}
+
+- (IBAction)sliderChangedC:(UISlider *)slider {
+	self.labelC.text = [NSString stringWithFormat:@"%.0f Hz", slider.value];
+}
+
+- (IBAction)sliderChangedD:(UISlider *)slider {
+	self.labelD.text = [NSString stringWithFormat:@"%.0f Hz", slider.value];
+}
+
+- (IBAction)sliderChangedE:(UISlider *)slider {
+	self.labelE.text = [NSString stringWithFormat:@"%.0f Hz", slider.value];
+}
+
 
 @end
